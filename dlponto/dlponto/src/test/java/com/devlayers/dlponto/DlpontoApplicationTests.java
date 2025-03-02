5f26a97eba5227319a1bf9850733d28262d9968a
Laís <162036972+Lys-31@users.noreply.github.com>
2025-03-02 00:05:40 -0300

    Inserindo primeiro projeto SpringBoot

---
+++ dlponto/dlponto/src/test/java/com/devlayers/dlponto/DlpontoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devlayers.dlponto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DlpontoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
