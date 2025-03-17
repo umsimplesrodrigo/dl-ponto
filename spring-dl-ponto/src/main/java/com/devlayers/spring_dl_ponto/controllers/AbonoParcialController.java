a1539ddc232d9f40d3db29c5440fd034406c076b
Laís <162036972+Lys-31@users.noreply.github.com>
2025-03-17 13:33:06 -0300

    Inserindo mais algumas classes

---
+++ spring-dl-ponto/src/main/java/com/devlayers/spring_dl_ponto/controllers/AbonoParcialController.java
@@ -0,0 +1,47 @@
+package com.devlayers.spring_dl_ponto.controllers;
+
+import com.devlayers.spring_dl_ponto.entities.AbonoParcial;
+import com.devlayers.spring_dl_ponto.entities.Funcao;
+import com.devlayers.spring_dl_ponto.facades.AbonoParcialFacade;
+import com.devlayers.spring_dl_ponto.facades.FuncaoFacade;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/abonosParciais")
+public class AbonoParcialController {
+
+    private final AbonoParcialFacade abonoParcialFacade;
+
+    @Autowired
+    public AbonoParcialController(AbonoParcialFacade abonoParcialFacade) {
+        this.abonoParcialFacade = abonoParcialFacade;
+    }
+
+    @GetMapping("")
+    public List<AbonoParcial> get() {
+        return abonoParcialFacade.search();
+    }
+
+    @GetMapping("/{id}")
+    public AbonoParcial getAbonosParciais(@PathVariable int id) {
+        return abonoParcialFacade.searchById(id);
+    }
+
+    @PostMapping("")
+    public void register(@RequestBody AbonoParcial abonoParcial) {
+        abonoParcialFacade.cadastrar(abonoParcial);
+    }
+
+    @PutMapping("/{id}")
+    public void update(@PathVariable int id, @RequestBody AbonoParcial abonoParcial) {
+        abonoParcialFacade.atualizar(id, abonoParcial);
+    }
+
+    @DeleteMapping("/{id}")
+    public void remove(@PathVariable int id) {
+        abonoParcialFacade.deletar(id);
+    }
+}
