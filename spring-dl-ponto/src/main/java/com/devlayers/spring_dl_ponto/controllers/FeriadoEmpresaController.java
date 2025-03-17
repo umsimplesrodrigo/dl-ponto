9e1a00e4936f90eb304a63721c4bd3347d0f1b40
Aluno Feira <aluno.fsa@senaiba.com.br>
2025-03-17 20:01:16 -0300

    Outro pequeno ajuste

--- spring-dl-ponto/src/main/java/com/devlayers/spring_dl_ponto/controllers/FeriadoEmpresaController.java
+++ spring-dl-ponto/src/main/java/com/devlayers/spring_dl_ponto/controllers/FeriadoEmpresaController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/feriadosEmpresas")
+@RequestMapping("/api/feriados-empresas")
 public class FeriadoEmpresaController {
 
     private final FeriadoEmpresaFacade feriadoEmpresaFacade;
