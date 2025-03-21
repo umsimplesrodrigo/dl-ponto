9b70c0ad3366fc963e98e52f239fd75d681f4a10
ELPATON <tetheu.10.alcantarasilva@gmail.com>
2025-03-20 23:45:57 -0300

    Refatora entidade Feriado para usar Jakarta Persistence

--- spring-dl-ponto/src/main/java/com/devlayers/spring_dl_ponto/entities/Afastamento.java
+++ spring-dl-ponto/src/main/java/com/devlayers/spring_dl_ponto/entities/Afastamento.java
@@ -1,15 +1,45 @@
 package com.devlayers.spring_dl_ponto.entities;
 
+import jakarta.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "afastamentos")
 public class Afastamento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "funcionario_id", nullable = false)
     private Funcionario funcionario;
-    private LocalDate data_inicio;
-    private LocalDate data_fim;
+
+    @Column(name = "data_inicio", nullable = false)
+    private LocalDate dataInicio;
+
+    @Column(name = "data_fim")
+    private LocalDate dataFim;
+
+    @ManyToOne
+    @JoinColumn(name = "justificativa_id", nullable = false)
     private Justificativa justificativa;
+
+    @Column(name = "observacao", length = 255)
     private String obs;
 
+    // Construtores
+    public Afastamento() {}
+
+    public Afastamento(Funcionario funcionario, LocalDate dataInicio, LocalDate dataFim, Justificativa justificativa, String obs) {
+        this.funcionario = funcionario;
+        this.dataInicio = dataInicio;
+        this.dataFim = dataFim;
+        this.justificativa = justificativa;
+        this.obs = obs;
+    }
+
+    // Getters e Setters
     public Long getId() {
         return id;
     }
@@ -26,20 +56,20 @@ public class Afastamento {
         this.funcionario = funcionario;
     }
 
-    public LocalDate getData_inicio() {
-        return data_inicio;
+    public LocalDate getDataInicio() {
+        return dataInicio;
     }
 
-    public void setData_inicio(LocalDate data_inicio) {
-        this.data_inicio = data_inicio;
+    public void setDataInicio(LocalDate dataInicio) {
+        this.dataInicio = dataInicio;
     }
 
-    public LocalDate getData_fim() {
-        return data_fim;
+    public LocalDate getDataFim() {
+        return dataFim;
     }
 
-    public void setData_fim(LocalDate data_fim) {
-        this.data_fim = data_fim;
+    public void setDataFim(LocalDate dataFim) {
+        this.dataFim = dataFim;
     }
 
     public Justificativa getJustificativa() {
@@ -58,3 +88,4 @@ public class Afastamento {
         this.obs = obs;
     }
 }
+
--- spring-dl-ponto/src/main/java/com/devlayers/spring_dl_ponto/entities/Feriado.java
+++ spring-dl-ponto/src/main/java/com/devlayers/spring_dl_ponto/entities/Feriado.java
@@ -1,12 +1,32 @@
 package com.devlayers.spring_dl_ponto.entities;
 
+import jakarta.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "feriados")
 public class Feriado {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "descricao", nullable = false, length = 100)
     private String descricao;
+
+    @Column(name = "data", nullable = false)
     private LocalDate data;
 
+    // Construtores
+    public Feriado() {}
+
+    public Feriado(String descricao, LocalDate data) {
+        this.descricao = descricao;
+        this.data = data;
+    }
+
+    // Getters e Setters
     public Long getId() {
         return id;
     }
@@ -31,3 +51,4 @@ public class Feriado {
         this.data = data;
     }
 }
+
