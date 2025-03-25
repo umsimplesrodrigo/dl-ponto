1a0bf60348e4d1bb0d31f95eede7e3bdd74a52d2
umsimplesrodrigo <umsimplesti@gmail.com>
2025-03-25 16:14:31 -0300

    Posts completos com erro ainda

--- spring-dl-ponto/src/main/java/com/devlayers/spring_dl_ponto/repositories/jpa/FuncionarioJPA.java
+++ spring-dl-ponto/src/main/java/com/devlayers/spring_dl_ponto/repositories/jpa/FuncionarioJPA.java
@@ -2,6 +2,8 @@ package com.devlayers.spring_dl_ponto.repositories.jpa;
 
 import com.devlayers.spring_dl_ponto.entities.Funcionario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FuncionarioJPA extends JpaRepository<Funcionario, Long> {
 }
